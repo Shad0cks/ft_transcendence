9e640f30168c0519dae93ed001e6f905b1522664
Gregory ARTAUD <gregory.rtd@gmail.com>
2022-11-04 18:01:05 +0100

    getUserInfo function

--- ui/src/services/User/getUserInfo.ts
+++ ui/src/services/User/getUserInfo.ts
@@ -1,4 +1,9 @@
 export function GetUserInfo(id: string) {
-    return fetch(process.env.REACT_APP_API_URL + '/user/' + id);
-  }
-  
+  return fetch(process.env.REACT_APP_API_URL + '/user/' + id, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+    credentials: 'include',
+  });
+}
