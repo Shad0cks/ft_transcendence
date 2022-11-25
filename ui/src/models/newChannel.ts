e9272d1142a4d304121b967d931d6db173b07093
Pierre-Louis <pierrelouisdeshayes42@gmail.com>
2022-11-25 19:13:07 +0100

    createchannel ui

---
+++ ui/src/models/newChannel.ts
@@ -0,0 +1,8 @@
+import { ChannelPrivacyType } from "./channelPrivacyType";
+
+export interface CreateChannelDTO {
+    channelName: string;
+    creatorNickname: string;
+    privacy: ChannelPrivacyType;
+    password: string;
+  }
--- ui/src/pages/HomePage/partyCreate.tsx
+++ ui/src/pages/HomePage/partyCreate.tsx
@@ -5,45 +5,60 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { Channel } from '../../models/channel';
 import { createChannel } from '../../services/Channel/createChannel';
 import TSSnackbar from '../../components/TSSnackbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom/dist';
+import socketIOClient, { Socket } from 'socket.io-client';
+import { CreateChannelDTO } from '../../models/newChannel';
 
 export default function PartyCreate({
   username,
 }: {
   username: string | undefined;
 }) {
-  const { register, watch, handleSubmit } = useForm<Channel>();
+  const { register, watch, setValue, handleSubmit } = useForm<CreateChannelDTO>();
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] =
     useState<AlertColor>('success');
+  const [socket, setSocket] = useState<Socket>();
 
-  const channelVisiblity = watch('restriction');
+  const channelVisiblity = watch('privacy');
   const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<Channel> = (data: Channel) => {
+  useEffect(()=>{
+    setSocket(
+      socketIOClient('http://localhost:8080', { withCredentials: true }),
+    );
+  }, [])
+
+  const onSubmit: SubmitHandler<CreateChannelDTO> = (data: CreateChannelDTO) => {
+    data.creatorNickname = username!
     console.log(data);
 
-    createChannel(data)
-      .then((res) => {
-        if (res.ok) {
-          const requete = res.text().then((e) => JSON.parse(e));
-          requete.then((e) => {
-            navigate('/game_' + e.id);
-          });
-        } else {
-          setSnackbarMessage('Error while creating channel.');
-          setSnackbarSeverity('error');
-          setOpenSnackbar(true);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        setSnackbarMessage('Error while creating channel.');
-        setSnackbarSeverity('error');
-        setOpenSnackbar(true);
-      });
+    if (socket && socket.id !== undefined) {
+      console.log("emit channel")
+      socket.emit('createChannel', data);
+    }
+    
+    // createChannel(data)
+    //   .then((res) => {
+    //     if (res.ok) {
+    //       const requete = res.text().then((e) => JSON.parse(e));
+    //       requete.then((e) => {
+    //         navigate('/game_' + e.id);
+    //       });
+    //     } else {
+    //       setSnackbarMessage('Error while creating channel.');
+    //       setSnackbarSeverity('error');
+    //       setOpenSnackbar(true);
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     setSnackbarMessage('Error while creating channel.');
+    //     setSnackbarSeverity('error');
+    //     setOpenSnackbar(true);
+    //   });
   };
 
   return (
@@ -55,12 +70,12 @@ export default function PartyCreate({
           <Form.Control
             id="gameName"
             placeholder="Wanderful name"
-            {...register('name')}
+            {...register('channelName')}
           />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label htmlFor="disabledSelect">Channel visibility</Form.Label>
-          <Form.Select id="disabledSelect" {...register('restriction')}>
+          <Form.Select id="disabledSelect" {...register('privacy')}>
             <option>protected</option>
             <option>public</option>
             <option>private</option>
