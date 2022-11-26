e08115301d7d182d941a11de295a29d15d4f78cc
Gregory ARTAUD <gregory.rtd@gmail.com>
2022-11-26 16:56:02 +0100

    fix style

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,6 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { CreateChannelDTO } from 'src/dto/createChannel.dto';
-import { ChannelDTO } from 'src/dto/channel.dto';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Channel } from 'src/entities/channel.entity';
 import { ChatService } from '../services/chat.service';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
@@ -13,14 +11,6 @@ export class ChatController {
   @UseGuards(JwtAuthGuard)
   async findAll(): Promise<Channel[]> {
     return await this.chatService.findAll();
-  }
-
-  @Post('channels')
-  @UseGuards(JwtAuthGuard)
-  createChannelAction(
-    @Body() createChannelDTO: CreateChannelDTO,
-  ): Promise<ChannelDTO> {
-    return this.chatService.createChannel(createChannelDTO);
   }
 
   // TODO
