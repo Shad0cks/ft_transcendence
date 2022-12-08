9dfaa75e44d9d45fab8780ee635eac4a17eea1a1
Gregory ARTAUD <gregory.rtd@gmail.com>
2022-12-08 15:27:46 +0100

    fix typo

--- src/auth.module.ts
+++ src/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
-import { Intra42Strategy } from './strategies/Intra42.startegy';
+import { Intra42Strategy } from './strategies/Intra42.strategy';
 import { UserModule } from './user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/Jwt.strategy';
