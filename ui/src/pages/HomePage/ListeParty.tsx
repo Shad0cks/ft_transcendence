e365d8cdba20ea9a391bdac1438c12566fc34a30
Pierre-Louis <pierrelouisdeshayes42@gmail.com>
2023-01-25 18:30:59 +0100

    Finish

--- ui/src/pages/HomePage/ListeParty.tsx
+++ ui/src/pages/HomePage/ListeParty.tsx
@@ -218,7 +218,7 @@ export default function ListeParty({
                 maxLength={10}
               />
               <Button
-                variant="outline-dark"
+                variant="outline-primary"
                 id="button-addon2"
                 onClick={() => joinWithPass()}
               >
